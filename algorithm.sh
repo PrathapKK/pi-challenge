76feddbec5d9ddd167d64b07266ff9a2883bb16f
PrathapKK <prathapkk@gmail.com>
2023-12-10 10:23:02 -0600

    Update algorithm.sh

--- algorithm.sh
+++ algorithm.sh
@@ -4,3 +4,4 @@ echo "3.14159" | tee -a report.txt
 
 # UNCOMMENT THE FOLLOWING LINE TO GET A MORE PRECISE CALCULATION OF PI. ALSO REMOVE THE PREVIOUS ECHO STATEMENTS.
 # echo "scale=1000; 4*a(1)" | bc -l | tee report.txt
+#added a new comment, lets see if this get updated in the server
